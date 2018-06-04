90243df7aa246dad904f4ec8856f80c8a1031d18
thymelous <tim.labushev@gmail.com>
2018-06-04 11:01:24 +0300

    Faker::Time does not take h:m:s difference into account, which results in ended_on sometimes being earlier than started_on

--- events.rb
+++ events.rb
@@ -9,8 +9,9 @@ Event = Struct.new(:name, :started_on, :ended_on) do
           lambda { "#{Static.h2.sample} of #{Static.h3.sample}" }
           ]
         name = resource.sample.call
-        started_on = Faker::Time.between('1970-1-1'.to_datetime, DateTime.now, :day) - 1.hour
-        ended_on = Faker::Time.between(started_on, started_on + 2.days, :evening)
+        started_on = date_between('1970-1-1'.to_datetime, DateTime.now)
+        ended_on = date_between(started_on + 5.minutes, started_on + 2.days)
+        binding.pry if started_on > ended_on
         Event[name, started_on.to_s, ended_on.to_s]
       end
     end
--- utils.rb
+++ utils.rb
@@ -4,7 +4,14 @@ require 'active_support/all'
 Time::DATE_FORMATS[:default] = '%F %T' # ISO8601 without a timezone
 
 def date_between(start_date, end_date)
-  Faker::Time.between(start_date.to_datetime, end_date.to_datetime)
+  start_date, end_date = [start_date, end_date].map(&:to_datetime)
+  date = Faker::Date.between(start_date.to_datetime, end_date.to_datetime)
+  sample_from = if date == start_date.to_date then start_date + 1.minute else start_date end
+  sample_to = if date == end_date.to_date then end_date else DateTime.new(1970, 1, 1, 23, 59, 59) end
+  h, m, s = [(sample_from.hour..sample_to.hour), (sample_from.minute..sample_to.minute), (sample_from.second..sample_to.second)].map do |range|
+    if range.min.nil? then (range.last..range.first).sample else range.sample end
+  end
+  DateTime.new(date.year, date.month, date.day, h, m, s)
 end
 
 class Integer
@@ -33,10 +40,10 @@ class DateRangeGenerator
   def gen(from, offset)
     departed_on, returned_on = if 90.percent_chance
         departed_on = date_between(from, Date.today)
-        returned_on = Faker::Time.between(departed_on, departed_on.to_time + offset, :all)
+        returned_on = date_between(departed_on, departed_on.to_time + offset)
         [departed_on, returned_on]
       else
-        [Faker::Time.between(offset.ago, Date.today, :all), nil]
+        [date_between(offset.ago, Date.today), nil]
       end
     return gen(from, offset) if @limited_to == :even_years && departed_on.year.odd? || @limited_to == :odd_years && departed_on.year.even?
     return gen(from, offset) if @occupied_ranges.any? { |v| includes?(v, departed_on) || includes?(v, returned_on) }
