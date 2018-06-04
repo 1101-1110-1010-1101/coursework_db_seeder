930c9ea1397c930f13a548fc972fa0ed5e5c092c
Johnny Kekker <giomail.iv@gmail.com>
2018-06-04 07:19:35 +0300

    find and remove issue with creatures generation

--- creatures.rb
+++ creatures.rb
@@ -1,10 +1,18 @@
 Creature = Struct.new(:name, :mom_classification, :discovered_on) do
   class << self
     def get_creatures(count)
+      i_array = []
+      if count > Static.creatures.size
+        count = Static.creatures.size
+      end
       Array.new(count) do
-      name, mom_classification = Static.creatures.sample.split(':')
-      discovered_on = Faker::Time.backward(100000, :all).to_s[0..-7]
-      Creature[name, mom_classification, discovered_on]
+        i = Random.rand(0..Static.creatures.size)
+        while i_array.any? { |v| v == i }
+          i = Random.rand(0..Static.creatures.size)
+        end
+        name, mom_classification = Static.creatures.sample.split(':')
+        discovered_on = Faker::Time.backward(100000, :all).to_s[0..-7]
+        Creature[name, mom_classification, discovered_on]
       end
     end
   end
